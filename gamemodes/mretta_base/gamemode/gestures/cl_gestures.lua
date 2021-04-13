f954ec25a8850fc717bb03ce64c22e09d40e4f99
TW1STaL1CKY <tw1stal1cky@gmail.com>
2021-04-14 00:19:10 +0100

    Added mretta.PlayPlayerGesture helper function for easy networked gestures

---
+++ gamemodes/mretta_base/gamemode/gestures/cl_gestures.lua
@@ -0,0 +1,15 @@
+module("mretta",package.seeall)
+
+function PlayPlayerGesture(pl,slot,seqId)
+	assert(pl and pl:IsValid() and pl:IsPlayer(),"valid player expected for argument #1")
+
+	pl:AddVCDSequenceToGestureSlot(slot,seqId,0,true)
+end
+
+net.Receive("mretta_gestures",function()
+	local pl = net.ReadEntity()
+	local slot = net.ReadUInt(3)
+	local seqId = net.ReadUInt(12)
+
+	PlayPlayerGesture(pl,slot,seqId)
+end)
---
+++ gamemodes/mretta_base/gamemode/gestures/sv_gestures.lua
@@ -0,0 +1,20 @@
+module("mretta",package.seeall)
+
+local nwTag = "mretta_gestures"
+
+util.AddNetworkString(nwTag)
+
+function PlayPlayerGesture(pl,slot,seqId,omitPl)
+	assert(pl and pl:IsValid() and pl:IsPlayer(),"valid player expected for argument #1")
+
+	net.Start(nwTag)
+	net.WriteEntity(pl)
+	net.WriteUInt(slot,3)
+	net.WriteUInt(seqId,12)
+
+	if omitPl then
+		net.SendOmit(pl)
+	else
+		net.SendToServer()
+	end
+end
